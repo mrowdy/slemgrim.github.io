61e42e48f87d4693a712a157bf7dd270d9b5d12b
Slemgrim <slemgrim@gmail.com>
2014-07-18 17:36:09 +0200

    sidebar animation

--- web/assets/scripts/index.dart
+++ web/assets/scripts/index.dart
@@ -4,15 +4,20 @@ import 'package:slemgrim.com/slemgrim.dart';
 
 Element container = querySelector('#container');
 Element sidebar = querySelector('.sidebar');
+Element sidebarToggle = querySelector('.sidebarToggle');
 
 main(){
 
     container.classes.add('init');
 
     new Future.delayed(new Duration(milliseconds: 500)).then(initSidebar);
+
+    sidebarToggle.onClick.listen((_){
+        print('click');
+        sidebar.classes.toggle('show');
+    });
 }
 
 void initSidebar(Future future){
     sidebar.classes.add('init');
-
 }
